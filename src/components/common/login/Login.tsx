b0e796a5172ac2126bd09280e0211403b9c646f0
vietnq <>
2025-08-14 14:34:05 +0700

    add connect api

--- src/components/common/login/Login.tsx
+++ src/components/common/login/Login.tsx
@@ -1,10 +1,20 @@
+import { useEffect } from "react";
+import { login } from "../../../api/auth";
+
 export default function Login() {
-  return (
-    <div style={{ background: "#fff", padding: 20, borderRadius: 8 }}>
-      <h2>Login</h2>
-      <input type="text" placeholder="Username" /><br />
-      <input type="password" placeholder="Password" /><br />
-      <button>Login</button>
-    </div>
-  );
+
+    useEffect(() => {
+        login({ email: "test@gmail.com", password: "123456" })
+            .then(res => console.log(res.data))
+            .catch(err => console.error(err));
+    }, []);
+
+    return (
+        <div style={{ background: "#fff", padding: 20, borderRadius: 8 }}>
+            <h2>Login</h2>
+            <input type="text" placeholder="Username" /><br />
+            <input type="password" placeholder="Password" /><br />
+            <button>Login</button>
+        </div>
+    );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
-  return (
-    <header style={{ padding: "10px", background: "#f5f5f5" }}>
-      <nav>
-        <Link to="/" style={{ marginRight: 10 }}>Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-    </header>
-  );
+    const apiBase = import.meta.env.VITE_BACKEND_API_BASE;
+    console.log("API Base URL:", apiBase);
+    return (
+        <header style={{ padding: "10px", background: "#f5f5f5" }}>
+            <nav>
+                <Link to="/" style={{ marginRight: 10 }}>Home</Link>
+                <Link to="/about">About</Link>
+            </nav>
+        </header>
+    );
 }
