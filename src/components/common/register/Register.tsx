441946999699995da2535bb40d0d7d28c5b24a91
vietnq <vietnq@nissho-vn.com>
2025-09-29 22:18:39 +0700

    change setting component

--- src/components/common/register/Register.tsx
+++ src/components/common/register/Register.tsx
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import { register } from "../../../api/auth";
 import "./Register.css";
+import { useTranslation } from "react-i18next";
 
 export default function Register() {
+
+  const { t } = useTranslation();
+
   useEffect(() => {
     register({ email: "test@gmail.com", password: "123456", name: "Test" })
       .then((res) => console.log(res.data))
@@ -10,10 +14,15 @@ export default function Register() {
   }, []);
 
   return (
-    <div>
-      <div className="register-container">
-        <div>
-            123
+    <div className="page-content">
+      <div className="login-container">
+        <div className="login-logo">
+        </div>
+        <div className="login-form">
+          <div className="login-title">{t("register")}</div>
+          <div className="form-container">
+            <div className=""></div>
+          </div>
         </div>
       </div>
     </div>
